6ba0409ccb0ab4facad58115c8c910d66b68333e
hwangminju <mjhwang96@gmail.com>
2018-12-21 11:49:29 +0900

    add Interface.java

---
+++ myJava/src/myJava/Interface.java
@@ -0,0 +1,25 @@
+package myJava;
+
+interface Animal { public abstract void cry(); }
+
+class Cat implements Animal {
+    public void cry() {
+        System.out.println("냐옹냐옹!");
+    }
+}
+
+class Dog implements Animal {
+    public void cry() {
+        System.out.println("멍멍!");
+    }
+}
+
+public class Interface {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        Dog d = new Dog();
+ 
+        c.cry();
+        d.cry();
+    }
+}
